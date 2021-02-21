cf6d01724b3decf94c99b39d77824b4862ea8ae7
whoknowscode <luispcode@gmail.com>
2021-02-21 11:51:57 -0400

    Heroku Enviroment

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ process.env.PORT = process.env.PORT || 8080;
 // Enviroment   
 // =================
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; // Default as "production"
 
 // =================
 // Database URI
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/coffe';
 } else {
-    urlDB = 'mongodb+srv://PolarProjectDB:12112001lpse@cluster0.f3fik.mongodb.net/coffe';
+    urlDB = process.env.MONGO_URI; // From Heroku
 }
 
 process.env.URLDB = urlDB;
